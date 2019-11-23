a5f3b66b7e2fe834191a75b52de73433adc57bc0
Alayna T <40475864+chonyonon@users.noreply.github.com>
2019-11-22 22:50:27 -0600

    Add auto, elevator, fix deadband method, etc
    Added auto code - not tested
    Added simple elevator stuff (like one line lol) - not tested
    Fixed deadband function (im dumb lol)
    Made backup (why don't I just use the CURRENT VERSION AAAA)

---
+++ Bartworst.java
@@ -0,0 +1,38 @@
+
+
+@Autonomous (name = "Bartworst" group = "Linear Opmode")
+public class Bartworst extends Bologna {
+
+    double distanceDriven;
+
+    @Override
+    public void runOpMode() {
+        
+    }
+
+    public void setAll(double power) { //set all motors to same speed, enter speed
+        leftUpper.setPower(power);
+        rightLower.setPower(power);
+        leftLower.setPower(power);
+        rightUpper.setPower(power);
+    }
+
+    public void driveUntil(double DISTANCE) {
+        distanceDriven = frontRight.getCurrentPosition();
+        while(distanceDriven < DISTANCE) {
+            setAll(.5);
+        } 
+        setAll(0);
+    }
+ //ticks = x * inches
+ //inches = ticks / x
+    public double toInches() {
+        return (frontRight.getCurrentPosition / conversion);
+    }
+
+    public double toTicks(double inches) {
+        return (conversion * inches);
+    }
+
+    
+}
--- bologna.java
+++ bologna.java
@@ -27,19 +27,20 @@ import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 
-
-@TeleOp(name="Bologna", group="Linear Opmode")
+@TeleOp(name = "Bologna", group = "Linear Opmode")
 
 public class Bologna extends LinearOpMode {
-    
+
     private double deadband(double initialVal, double deadband) {
-        if ( (initialVal > deadband) || (initialVal < -deadband) ) {
-            return 0;
-        } else {
+        if ((initialVal > deadband) || (initialVal < -deadband)) {
             return initialVal;
+        } else {
+            return 0;
         }
-    } 
-    
+    }
+
+    protected double conversion; //CONVERSION NUMBER FROM TICKS>INCHES AND VICE VERSA, IMPORTANT !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
     private double fl;
     private double bl;
     private double fr;
@@ -48,8 +49,8 @@ public class Bologna extends LinearOpMode {
     private double leftX;
     private double rightY;
     private double rightX;
-    private double myOpen = 100.0;
-    private double myClosed = 90.0;
+    protected double myOpen = 100.0;
+    protected double myClosed = 90.0;
 
     private boolean leftStick;
     private boolean rightStick;
@@ -57,68 +58,57 @@ public class Bologna extends LinearOpMode {
     private boolean leftTriggerGo;
     private boolean rightTriggerGo;
 
-    private static DcMotor frontLeft  = null;
-    private static DcMotor frontRight = null;
-    private static DcMotor backLeft  = null;
-    private static DcMotor backRight = null;
-    private Servo clawLeft = null;
-    private Servo clawRight = null;
-
-    enum PowerLevel {MAX, HALF, QUARTER, STOP}; 
-    private Orientation angles;
+    protected static DcMotor frontLeft = null;
+    protected static DcMotor frontRight = null;
+    protected static DcMotor backLeft = null;
+    protected static DcMotor backRight = null;
+    protected static DcMotor elevatorMotor = null; 
+    protected Servo clawLeft = null;
+    protected Servo clawRight = null;
 
     // Declare OpMode members/constants.
     private ElapsedTime runtime = new ElapsedTime();
 
     private static double MOTOR_ADJUST = 0.60;
-    
+
     @Override
     public void runOpMode() {
-        
-        PowerLevel powerLevel = PowerLevel.MAX;     //Starts the robot wheels at MAX power level
-        
-        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         clawLeft = hardwareMap.get(Servo.class, "clawLeft");
         clawRight = hardwareMap.get(Servo.class, "clawRight");
-                
+        elevatorMotor = hardwareMap.get(DcMotor.class, "elevatorMotor");
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
 
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
-        
-         //Brake immedietly after joystick hits 0 instead of coasting down
+
+        // Brake immedietly after joystick hits 0 instead of coasting down
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // initIMU();
 
-        //initIMU();
-            
- 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-//------------------------------------------------------------------- Start of Match ---------------------------------------------------
+        // ------------------------------------------------------------------- Start of
+        // Match ---------------------------------------------------
         runtime.reset();
 
-        // Setup a variable for each drive wheel
-                    double leftUpperPower = 0;
-                    double rightUpperPower = 0;
-                    double leftLowerPower = 0;
-                    double rightLowerPower = 0;
-        
         while (opModeIsActive()) {
-            
-    //Moving the Bot        
-        //Mecanum drive
-        
+
+            // Moving the Bot
+            // Mecanum drive
+
             double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
             double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
             double rightX = -gamepad1.right_stick_x;
@@ -128,24 +118,28 @@ public class Bologna extends LinearOpMode {
             final double v4 = r * Math.cos(robotAngle) - rightX;
 
             if(gamepad1.y) {
-                MOTOR_ADJUST *= .5;
+                MOTOR_ADJUST = .5;
             } else if(gamepad1.a) {
-                MOTOR_ADJUST *= .2;
+                MOTOR_ADJUST = .2;
+            } else {
+                MOTOR_ADJUST = .6;
             }
-            
-            leftUpper.setPower(v1*MOTOR_ADJUST);
-            rightLower.setPower(v2*MOTOR_ADJUST);
-            leftLower.setPower(v3*MOTOR_ADJUST);
-            rightUpper.setPower(v4*MOTOR_ADJUST);
 
-    //Weapons Things
-        //Claw Controls
-            if(gamepad2.left_trigger > 0) {
+            frontLeft.setPower(v1*MOTOR_ADJUST);
+            backRight.setPower(v2*MOTOR_ADJUST);
+            backLeft.setPower(v3*MOTOR_ADJUST);
+            frontRight.setPower(v4*MOTOR_ADJUST);
+
+            // Weapons Things
+            // Claw Controls
+            if (gamepad2.left_trigger > 0) {
                 leftTriggerGo = true;
             }
-            if(gamepad2.right_trigger > 0) {
+            if (gamepad2.right_trigger > 0) {
                 rightTriggerGo = true;
             }
+
+            elevatorMotor.setPower(deadband(gamepad2.right_stick_y , .03));
 
             if (rightTriggerGo) {
                 clawLeft.setPosition(myOpen);
@@ -157,10 +151,12 @@ public class Bologna extends LinearOpMode {
                 clawRight.setPosition(clawRight.getDirection);
                 clawLeft.setPosition(clawLeft.getDirection);
             }
-        telemetry.addData("Motor Power:", "(%.2f) (%.2f) (%.2f) (%.2f)", fl,fr,bl,br);
-        telemetry.addData("Predicted Motor Speed:");
-        telemetry.update();
+            telemetry.addData("Motor Power:", "(%.2f) (%.2f) (%.2f) (%.2f)", fl, fr, bl, br);
+            telemetry.addData("Predicted Motor Speed:", "(%.2f) (%.2f) (%.2f) (%.2f)" , v1 * MOTOR_ADJUST, v2 * MOTOR_ADJUST, v3 * MOTOR_ADJUST, v4 * MOTOR_ADJUST);
+            telemetry.addData("Claw Servo Degrees:", "(%.2f)", clawRight.getDirection);
+            telemetry.addData("Ticks moved :", "(%.2f)", )
+            telemetry.update();
         }
     }
-    
+
 }
